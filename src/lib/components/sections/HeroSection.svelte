8b4f65f77d5cb4ad3445d94f9fd549f168961844
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2025-04-07 20:05:57 +0200

    creating hero section

---
+++ src/lib/components/sections/HeroSection.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { goto } from "$app/navigation";
+  import Button from "../UI/Button.svelte";
+
+  const handleClick = () => {
+    goto("/#contact-form")
+  }
+
+</script>
+<div class="hero-section default-margin">
+  <h3>Hello, I'm Chris</h3>
+  <div class="underscore mb-b"></div>
+  <h1>I am a Svelte developer crafting intuitve and performant</h1>
+  <h1 class="dark-grey mb-m">Web experiences.</h1>
+  <Button onclick={handleClick}>Let's Talk</Button>
+</div>
+
+<style>
+  .hero-section {
+    padding-top: 60px;
+  }
+</style>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as HeroSection } from './components/sections/HeroSection.svelte';
+export { default as Button } from './components/ui/Button.svelte';
