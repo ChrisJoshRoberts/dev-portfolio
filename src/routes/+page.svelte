228f1bab603c1039d4075d5514de091e016bcb6d
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2025-04-08 13:16:37 +0200

    connecting sanity CMS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
   import HeroSection from "$components/sections/HeroSection.svelte";
   import AboutSection from "$components/sections/AboutSection.svelte";
   import ExperienceSection  from "$components/sections/ExperienceTable.svelte";
+
+  const data = $props();
+  $inspect(data)
 </script>
 
 <HeroSection />
---
+++ src/routes/+page.ts
@@ -0,0 +1,10 @@
+import sanityClient from "$utils/sanity";
+import type { PageLoad } from "./$types";
+import type { WorkExperience } from "$lib/types/sanity";
+
+export const load: PageLoad = async () => {
+  const workExpData: WorkExperience[] = await sanityClient.fetch(
+    `*[_type == "workExperience"] | order(StartDate desc)` 
+  );
+  return { workExpData};
+}
