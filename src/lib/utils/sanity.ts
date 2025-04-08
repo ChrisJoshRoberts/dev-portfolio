9a3a9b57bc290eea47db18a94021fb332a4217ad
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2025-04-08 12:47:55 +0200

    adding sanity

---
+++ src/lib/utils/sanity.ts
@@ -0,0 +1,12 @@
+import { createClient, type ClientConfig } from '@sanity/client';
+
+const config: ClientConfig = {
+  projectId: '5djtiqyl',
+  dataset: 'production', 
+  useCdn: true,
+  apiVersion: '2025-05-08'
+};
+
+const sanityClient = createClient(config);
+
+export default sanityClient;
